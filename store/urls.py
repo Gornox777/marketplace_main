cd17c0a12194211e4bac3bfca2e7d444aef8ea9c
Gornox777 <joseluisglopez2@gmail.com>
2025-11-07 11:03:21 -0800

    Add files via upload

---
+++ store/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+
+from.views import contact, detail
+
+
+urlpatterns = [
+    path('contact/',contact, name='contact' ),
+    path('detail/<int:pk>', detail, name='detail'),
+]
